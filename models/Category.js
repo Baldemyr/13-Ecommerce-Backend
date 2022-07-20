4b1a1644715795949421ed2d7115fcacd93ebefa
Baldemyr <son_of_loki@hotmail.com>
2022-07-19 22:23:36 -0400

    Followed TODOs in models

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,18 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+// define columns
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
@@ -18,3 +29,5 @@ Category.init(
 );
 
 module.exports = Category;
+
+
